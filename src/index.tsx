b80110c32e604f102849e4639611156b60ab3786
柯忠发 <zhongfa.ke@shuyun.com>
2020-07-20 10:07:06 +0800

    1111

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,2 @@
 export { default as Button } from "./components/button";
-export { default as MultipleEnterInputSelect } from "./components/button";
+export { default as MultipleEnterInputSelect } from "./components/multiple-enter-input-select";
